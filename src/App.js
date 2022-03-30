e39465086eb56bfb8b46aeb36184c306eeaeb0ad
vivek <vivekrsq@gmail.com>
2022-03-30 15:45:37 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Header from "./components/Header";
+import { useState } from "react";
+import {Buffer} from 'buffer';
+Buffer.from('anything','base64');
 function App() {
+  const [records, setRecords] = useState({
+    type: "",
+    host: "",
+    value: "",
+    ttl: ""
+  })
+
+  let onChangeHandler = (e)=>{
+    const name = e.target.name;
+    const value = e.target.value;
+    setRecords({...records, [name] : value})
+  }
+
+
+let handleSubmit = (e)=>{
+  e.preventDefault();
+  const credentials = Buffer.from(`587a87d68d13ce981eb3fd6753f30d1b96489a51cc002e917556c17ee4292960:c8064a8641bd513fb05f3f1df9f1d3efda61d912447f550bc8d28ff508997c2e`);
+  const encodedCredentials = credentials.toString('base64');
+  const authorization = `Basic ${encodedCredentials}`;
+  fetch('https://www.namebase.io/api/v0/dns/domains/wowman/nameserver', {
+    method: 'GET',
+    headers: {
+      Authorization: authorization,
+      
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+  })
+  .then(res=>res.json())
+  .then(data=>console.log(data))
+  console.log(records);
+}
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <Header />
+    <div className="container my-4">
+    <form className="row g-3" onSubmit={handleSubmit}>
+  
+  <div className="col-md-2">
+    <label htmlFor="inputCity" className="form-label">IP</label>
+    <input type="text" className="form-control" value={records.value} onChange={onChangeHandler} name="value" id="inputCity"/>
+  </div>
+  <div className="col-md-2">
+    <label htmlFor="inputState" className="form-label">Type</label>
+    <select id="inputState" className="form-select" value={records.type} onChange={onChangeHandler} name="type">
+      <option selected>Choose...</option>
+      <option value="A">A</option>
+    </select>
+  </div>
+  <div className="col-md-2">
+    <label htmlFor="inputZip" className="form-label">TTL</label>
+    <input type="text" className="form-control" value={records.ttl} onChange={onChangeHandler} name="ttl" id="inputZip"/>
+  </div>
+  <div className="col-md-2">
+    <label htmlFor="inputZip" className="form-label">Host</label>
+    <input type="text" className="form-control" value={records.host} onChange={onChangeHandler} name="host" id="inputZip2"/>
+  </div>
+  
+  <div className="col-12 d-flex">
+    <button type="submit" className="btn btn-primary">Update</button>
+  </div>
+</form>
+<button type="submit" className="btn btn-primary" onClick={handleSubmit}>check</button>
     </div>
+    </>
   );
 }
 
